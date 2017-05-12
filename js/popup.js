7522ec6681b1e0ae676a09820cf1026383218859
arthur <minasyanakk@outlook.com>
2017-05-12 18:08:08 +0300

    123

--- js/popup.js
+++ js/popup.js
@@ -4,8 +4,33 @@ function getword(info,tab) {
     url: "https://www.youtube.com/results?search_query=" + info.selectionText,
   });           
 }
+function bandcamp(info,tab) {
+  console.log("Word " + info.selectionText + " was clicked.");
+  chrome.tabs.create({  
+    url: "https://bandcamp.com/search?q=" + info.selectionText,
+  });           
+}
+function link(info,tab) {
+  console.log("Word " + info.selectionText + " was clicked.");
+  chrome.tabs.create({  
+    url: "https://www.youtube.com",
+  });           
+}
+
+
 chrome.contextMenus.create({
   title: "Поиск youtube: %s", 
   contexts:["selection"], 
   onclick: getword,
+});
+chrome.contextMenus.create({
+  title: "Поиск bandcamp: %s", 
+  contexts:["selection"], 
+  onclick: bandcamp,
+});
+
+chrome.contextMenus.create({
+  title: "Youtube", 
+  contexts:["all"], 
+  onclick: link,
 });
